88aeb1d70e8e82ad58941c50a045de69c155ba36
Max <mailmaxxx@gmail.com>
2018-05-08 19:02:41 +1000

    Added new ars-components library project

---
+++ projects/ars-components/src/lib/ars-components.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { ArsComponentsComponent } from './ars-components.component';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [ArsComponentsComponent],
+  exports: [ArsComponentsComponent]
+})
+export class ArsComponentsModule { }
